0d9f96a6aac196ab0dd8ab390134e61d598910a4
mimi <kmy930901@naver.com>
2023-05-21 22:13:01 +0900

    섹션1. API개발 기본 - V1: Entity 자체를 활용한 API - V2: 별도의 DTO를 활용한 API

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,66 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
+@RestController // @Controller + @ResponseBody - data 자체를 바로 xml나 json으로 보내자
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    /**
+     * 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다.
+     * 문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * - 엔티티에 API 검증을 위한 로직이 들어간다. (@NotEmpty 등등)
+     * - 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위한 모든 요청 요구사항을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * 결론
+     * - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
+     */
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { // json으로 온 body를 member에 그대로 매핑해줌.
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+    // Entity를 외부에 노출하지 말자.
+    // Entity를 파라미터로 받지 말자.
+
+    /**
+     * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다.
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest { // 별도의 DTO
+        // 필요한 필드만 사용 가능. API 스펙을 바로 알 수 있음.
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
