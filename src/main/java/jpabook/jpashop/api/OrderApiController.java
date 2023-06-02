e71171e820ad1982cf22610396d3d66a2d96f976
mimi <kmy930901@naver.com>
2023-06-03 08:36:56 +0900

    주문조회 V5: JPA에서 DTO 직접 조회 - 플랫 데이터 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
@@ -16,7 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -48,7 +51,7 @@ public class OrderApiController {
     public List<OrderDto> ordersV2() {
         return  orderRepository.findAllByString(new OrderSearch()).stream()
                 .map(OrderDto::new)
-                .collect(Collectors.toList());
+                .collect(toList());
     }
 
     @GetMapping("/api/v3/orders")
@@ -56,7 +59,7 @@ public class OrderApiController {
         // 중복 데이터가 너무 많음. DB에서 application으로 전송되는 데이터 용량이 많아짐.
         return  orderRepository.findAllWithItem().stream()
                 .map(OrderDto::new)
-                .collect(Collectors.toList());
+                .collect(toList());
     }
 
     @GetMapping("/api/v3.1/orders")
@@ -65,7 +68,7 @@ public class OrderApiController {
         // 쿼리는 더 많이 나가지만 정규화된 상태로 중복 데이터가 없음.
         return  orderRepository.findAllWithMemberDelivery(offset, limit).stream()
                 .map(OrderDto::new)
-                .collect(Collectors.toList());
+                .collect(toList());
     }
 
     @GetMapping("/api/v4/orders")
@@ -78,6 +81,23 @@ public class OrderApiController {
     public List<OrderQueryDto> orderV5() {
         return orderQueryRepository.findAllByDto_optimization();
     }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> orderV6() {
+        // 스펙을 맞추고 싶다. -> 노가다 필요
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        // 직접 중복을 거른다.
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList()))).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
+
+    }
+//    public List<OrderFlatDto> orderV6() {
+//        return orderQueryRepository.findAllByDto_flat();
+//    }
 
 
     @Getter
@@ -101,7 +121,7 @@ public class OrderApiController {
             orderItems = order.getOrderItems()
                     .stream()
                     .map(OrderItemDto::new)
-                    .collect(Collectors.toList());
+                    .collect(toList());
         }
     }
 
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -3,11 +3,13 @@ package jpabook.jpashop.repository.order.query;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId") // collect에서 groupBy 할때 기준으로 묶어준다.
 public class OrderQueryDto {
     // Controller 안에 있는 dto를 안쓰는 이유: repository가 controller를 참조하는 의존관계가 순환되버림.
 
@@ -24,5 +26,14 @@ public class OrderQueryDto {
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
